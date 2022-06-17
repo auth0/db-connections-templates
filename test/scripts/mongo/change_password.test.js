31e3d8f13f86edd0302ab57454dc269db46cde96
Anthony DeMartini <anthony.demartini@gmail.com>
2022-06-17 14:14:55 -0400

    fix

--- test/scripts/mongo/change_password.test.js
+++ test/scripts/mongo/change_password.test.js
@@ -58,7 +58,7 @@ describe(scriptName, () => {
       },
     }));
 
-    script('broken@example.com', (err, success) => {
+    script('duck.t@example.com', (err, success) => {
       expect(err).toBeFalsy();
       expect(success).toEqual(false);
       done();
