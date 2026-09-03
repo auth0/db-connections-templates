3c4ee12fc6c95ef1b4c265be1974d4cc300dcb16
Ben <ben.gimbel@okta.com>
2026-09-03 15:50:15 -0500

    feat: install aws sdk v3 using S3Client and PutObjectCommand

--- deploy.js
+++ deploy.js
@@ -1,8 +1,8 @@
-const aws = require('aws-sdk');
 const scripts = require('./dbscripts.json');
 require('dotenv').config();
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
-(function () {
+(async function () {
   if (!process.env.S3_ACCESS_KEY || !process.env.S3_SECRET || !process.env.S3_REGION || !process.env.S3_BUCKET) {
     console.log('AWS S3 settings missing. S3_ACCESS_KEY, S3_SECRET, S3_REGION and S3_BUCKET settings are required.');
     process.exit(1);
@@ -10,29 +10,28 @@ require('dotenv').config();
 
   console.log(`Deploying dbscripts.json into ${process.env.S3_BUCKET} bucket...`);
 
-  const S3 = new aws.S3({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET,
-    sessionToken: process.env.S3_SESSION_TOKEN,
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET,
+      sessionToken: process.env.S3_SESSION_TOKEN,
+    },
     region: process.env.S3_REGION,
-    params: {
-      Bucket: process.env.S3_BUCKET
-    }
   });
 
-  const params = {
+  const params = new PutObjectCommand({
+    Bucket: process.env.S3_BUCKET,
     Body: JSON.stringify(scripts),
     Key: 'dbscripts/dbscripts.json',
     ContentType: 'application/json'
-  };
+  });
 
-  return S3.putObject(params, function(err) {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-
+  try {
+    await s3.send(params);
     console.log('dbscripts.json deployed successfully.');
     process.exit();
-  });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 })();
